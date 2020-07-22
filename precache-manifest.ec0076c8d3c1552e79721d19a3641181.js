41ea535e0d7b9d8184b7a1a15d35a38c038bea9f
AntonioAndraues <juniorbrgamers@gmail.com>
2020-07-22 13:15:42 -0300

    Updates

---
+++ precache-manifest.ec0076c8d3c1552e79721d19a3641181.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7da933e6207f3cc463c8a275005e634",
+    "url": "/Portifolio/index.html"
+  },
+  {
+    "revision": "e837e679f3ba3ba8de5a",
+    "url": "/Portifolio/static/css/main.c8d60a7a.chunk.css"
+  },
+  {
+    "revision": "2446ff83e111b541d8bd",
+    "url": "/Portifolio/static/js/2.c58f0edb.chunk.js"
+  },
+  {
+    "revision": "e837e679f3ba3ba8de5a",
+    "url": "/Portifolio/static/js/main.b51c1584.chunk.js"
+  },
+  {
+    "revision": "644accefd07f19fac7ed",
+    "url": "/Portifolio/static/js/runtime~main.ecc9c45b.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/Portifolio/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/Portifolio/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/Portifolio/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portifolio/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Portifolio/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/Portifolio/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/Portifolio/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/Portifolio/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/Portifolio/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "5314dab9b932b65cc40c86ef69aff264",
+    "url": "/Portifolio/static/media/aclonica-regular.5314dab9.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Portifolio/static/media/roboto-regular.11eabca2.ttf"
+  }
+]);
